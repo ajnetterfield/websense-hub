0893fde3a8119dfbbe43801b73c9c862b4798a31
Alastair Netterfield <ajnetterfield@outlook.com>
2015-03-02 20:18:54 +0800

    Removed unused JS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,7 +50,7 @@ module.exports = function(grunt) {
           'assets/less/*.less',
           'assets/less/**/*.less'
         ],
-        tasks: ['less:dev', 'autoprefixer:dev']
+        tasks: ['less:dev']
       },
     }
   });
